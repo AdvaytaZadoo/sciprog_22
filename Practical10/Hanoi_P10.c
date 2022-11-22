252fcfdb7c45bc5a0a605c71abafbc98f67c59c8
unknown <advayta08@gmail.com>
2022-11-22 18:47:35 +0000

    Codes for Practical10

---
+++ Practical10/Hanoi_P10.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+# include <stdlib.h>
+void hanoi (int n, int source, int destination, int inter) //Declaring and defining the function
+{
+    //Base Cases
+    if(n==1)
+    {
+        printf("Move disc from %d to %d\n", source, destination);
+    }
+    else
+    {
+        hanoi(n-1, source, inter, destination);
+        printf("Move disc from %d to %d\n", source, destination);
+        hanoi(n-1, inter, destination, source);
+    }
+}
+
+int main(void)
+{
+    int h = 3; // initializing the variables
+    printf("Solution for %d discs\n", h);
+    hanoi(h,1,3,2); //calling the function
+    return 0;
+}
